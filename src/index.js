998a6ab10edd8a49d08d06ecf6b0f556ad8687a6
Anirudh Sundar <anirudhsundar@hotmail.com>
2019-04-23 22:19:22 +0530

    Add zooming and panning controls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
+import OrbitControls from "three-orbitcontrols";
 
-let camera, scene, renderer;
+let camera, scene, renderer, controls;
 let geometry, material, mesh;
 
 init();
@@ -18,16 +19,28 @@ function init() {
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
+  controls = new OrbitControls(camera);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+  controls.rotateSpeed = 0.3;
+  controls.panSpeed = 0.3;
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+
+  //controls.update() must be called after any manual changes to the camera's transform
+  // camera.position.set(0, 20, 100);
+  // controls.update();
 }
 
 function animate() {
   requestAnimationFrame(animate);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  // mesh.rotation.x += 0.01;
+  // mesh.rotation.y += 0.02;
+  controls.update();
 
   renderer.render(scene, camera);
 }
