1abf11fea61b6f6eba9048bda5a3e54561d04041
Anirudh Sundar <anirudhsundar@hotmail.com>
2019-04-24 11:20:04 +0530

    Add rotating turbine

--- src/index.js
+++ src/index.js
@@ -1,34 +1,73 @@
 import * as THREE from "three";
 import OrbitControls from "three-orbitcontrols";
+import * as STLLoader from "three-stl-loader";
+
+import hubAsset from "./assets/hub.stl";
+import bladeAsset from "./assets/blade.stl";
 
 let camera, scene, renderer, controls;
-let geometry, material, mesh;
+let windTurbine;
 
 init();
 animate();
 
 function init() {
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-  camera.position.z = 1;
-
+  camera.position.z = 2.4;
   scene = new THREE.Scene();
-
-  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
-
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  windTurbine = new THREE.Group();
+  windTurbine.position.set(0, 0, 0);
 
   controls = new OrbitControls(camera);
   controls.enableDamping = true;
   controls.dampingFactor = 0.25;
-  controls.rotateSpeed = 0.3;
+  controls.rotateSpeed = 0.2;
   controls.panSpeed = 0.3;
 
+  let loader = new (STLLoader(THREE));
+  loader.load(hubAsset, function (geometry) {
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.01, 0.01, 0.01);
+    mesh.position.set(-0.074, 0, 0);
+    mesh.rotation.set(Math.PI/2, 0, 0);
+    windTurbine.add(mesh);
+  });
+
+  // Blade 1
+  loader.load(bladeAsset, function (geometry) {
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.01, 0.01, 0.01);
+    mesh.position.set(0.103, -0.055, 0.046);
+    mesh.rotation.set(Math.PI/2, Math.PI/2, 0);
+    windTurbine.add(mesh);
+  });
+
+  // Blade 2
+  loader.load(bladeAsset, function (geometry) {
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.01, 0.01, 0.01);
+    mesh.position.set(-0.1, -0.057, 0.046);
+    mesh.rotation.set(Math.PI/2, -Math.PI/6, 0);
+    windTurbine.add(mesh);
+  });
+
+  // Blade 3
+  loader.load(bladeAsset, function (geometry) {
+    let material = new THREE.MeshNormalMaterial();
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(0.01, 0.01, 0.01);
+    mesh.position.set(0, 0.118, 0.046);
+    mesh.rotation.set(Math.PI/2, -5 * Math.PI/6, 0);
+    windTurbine.add(mesh);
+  });
+
+  scene.add(windTurbine);
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-
 
   //controls.update() must be called after any manual changes to the camera's transform
   // camera.position.set(0, 20, 100);
@@ -37,11 +76,7 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
-
-  // mesh.rotation.x += 0.01;
-  // mesh.rotation.y += 0.02;
-  controls.update();
-
+  windTurbine.rotation.z += 0.2;
   renderer.render(scene, camera);
 }
 
